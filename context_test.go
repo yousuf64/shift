fdfec6b2582004884bec67705b7d8bc4be65e319
yousuf <yusuffaseel@gmail.com>
2023-03-13 21:19:28 +0530

    add context tests

---
+++ context_test.go
@@ -0,0 +1,31 @@
+package shift
+
+import (
+	"context"
+	"net/http"
+	"testing"
+)
+
+func TestContext_FromContext(t *testing.T) {
+	fooRoute := Route{
+		Params:   emptyParams,
+		Template: "/foo",
+	}
+	ctx := WithRoute(context.Background(), fooRoute)
+	route, ok := FromContext(ctx)
+
+	assert(t, ok, "expected to find a route context")
+	assert(t, route == fooRoute, "expected routes to have identical properties")
+}
+
+func TestContext_RouteOf(t *testing.T) {
+	abcRoute := Route{
+		Params:   nil,
+		Template: "/abc",
+	}
+	request, _ := http.NewRequest("GET", "/abc", nil)
+	ctx := WithRoute(request.Context(), abcRoute)
+	request = request.WithContext(ctx)
+
+	assert(t, RouteOf(request) == abcRoute, "expected routes to have identical properties")
+}
