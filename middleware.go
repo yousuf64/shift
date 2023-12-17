d34d28d4e15bb43cadf4e2684988707a33c40dfe
yousuf <yusuffaseel@gmail.com>
2023-12-17 22:10:41 +0530

    update recover middleware * write stack trace to os.Stderr * no longer returns stack trace to client * added RecoverWithWriter middleware

--- middleware.go
+++ middleware.go
@@ -1,15 +1,29 @@
 package shift
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"net/http"
-	"runtime/debug"
+	"os"
+	"runtime"
 )
 
 // Recover gracefully handle panics in the subsequent middlewares in the chain and the request handler.
-// It returns HTTP 500 (Internal Server Error) status and the stack trace.
+// It returns HTTP 500 ([http.StatusInternalServerError]) status and write the stack trace to [os.Stderr].
+//
+// Use [RecoverWithWriter] to write to a different [io.Writer].
 func Recover() MiddlewareFunc {
+	return RecoverWithWriter(os.Stderr)
+}
+
+// RecoverWithWriter gracefully handle panics in the subsequent middlewares in the chain and the request handler.
+// It returns HTTP 500 ([http.StatusInternalServerError]) status and write the stack trace to the provided [io.Writer].
+//
+// Use [Recover] to write to [os.Stderr].
+func RecoverWithWriter(w io.Writer) MiddlewareFunc {
 	return func(next HandlerFunc) HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request, route Route) (err error) {
+		return func(rw http.ResponseWriter, r *http.Request, route Route) error {
 			defer func() {
 				rec := recover()
 				switch rec {
@@ -18,14 +32,30 @@ func Recover() MiddlewareFunc {
 				case http.ErrAbortHandler:
 					panic(rec)
 				default:
-					w.WriteHeader(http.StatusInternalServerError)
-					_, _ = w.Write(debug.Stack())
+					writeStack(w, rec, 3)
+					rw.WriteHeader(http.StatusInternalServerError)
 				}
 			}()
 
-			return next(w, r, route)
+			return next(rw, r, route)
 		}
 	}
+}
+
+func writeStack(w io.Writer, rec any, skipFrames int) {
+	buf := &bytes.Buffer{}
+
+	for i := skipFrames; ; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+		f := runtime.FuncForPC(pc)
+		fmt.Fprintf(buf, "%s\n", f.Name())
+		fmt.Fprintf(buf, "	%s:%d (%#x)\n", file, line, pc)
+	}
+
+	fmt.Fprintf(w, "panic: %v\n%s", rec, buf.String())
 }
 
 // RouteContext packs Route information into http.Request context.
